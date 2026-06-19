40cb8e11c545e52f3541b21024eaacbc73e9a713
JMA <josemarceloandradealmeidas@gmail.com>
2026-06-18 21:22:44 -0300

    add cors

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ import express from "express";
 import {routerFilmes} from "./routes/routes.filme.js";
 import {errosController} from "./controllers/erros/controllers.erros.js";
 import {ErroNotFound} from "./classes/erros/class.NotFound.js";
+import cors from "cors";
 
 export const app = express();
 app.use(express.json({limit: "100kb"}));
+app.use(cors({origin: "*"}));
 app.use(routerFilmes);
 app.use((req, res, next) => next(new ErroNotFound("Rota não encontrada!")));
 app.use(errosController);
